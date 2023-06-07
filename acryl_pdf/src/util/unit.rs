f4f9069c5410c8b64ea4400013d34bc12fad834a
brndel <ar.braendle@gmail.com>
2023-06-07 19:45:18 +0200

    Add converters for Unit and Vector2

--- acryl_pdf/src/util/unit.rs
+++ acryl_pdf/src/util/unit.rs
@@ -1,13 +1,25 @@
-use std::ops::{Add, Sub, AddAssign, SubAssign};
+use std::{ops::{Add, Sub, AddAssign, SubAssign}, fmt::Display};
 
 use crate::render::PdfObj;
 
 #[derive(Clone, Copy, Debug, Default)]
 pub struct Pt(i64);
 
+impl Display for Pt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
 impl Into<PdfObj> for Pt {
     fn into(self) -> PdfObj {
         PdfObj::Int(self.0)
+    }
+}
+
+impl Into<Pt> for i64 {
+    fn into(self) -> Pt {
+        Pt(self)
     }
 }
 
@@ -36,11 +48,5 @@ impl Sub for Pt {
 impl SubAssign for Pt {
     fn sub_assign(&mut self, rhs: Self) {
         self.0 -= rhs.0
-    }
-}
-
-impl Into<Pt> for i64 {
-    fn into(self) -> Pt {
-        Pt(self)
     }
 }
--- acryl_pdf/src/util/vec.rs
+++ acryl_pdf/src/util/vec.rs
@@ -1,7 +1,28 @@
+use std::ops::Add;
+
+use crate::render::PdfObj;
+
 use super::Pt;
 
 #[derive(Clone, Default)]
 pub struct Vector2 {
     pub x: Pt,
     pub y: Pt,
+}
+
+impl Into<PdfObj> for Vector2 {
+    fn into(self) -> PdfObj {
+        PdfObj::Array(vec![self.x.into(), self.y.into()])
+    }
+}
+
+impl Add for Vector2 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
 }
