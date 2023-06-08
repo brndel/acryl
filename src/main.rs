cec921d0598e0e243d2ef6bae2e1c179f12e2b3c
brndel <ar.braendle@gmail.com>
2023-06-08 19:04:48 +0200

    Fix main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::fs;
 
-use acryl_pdf::{Document, Vector2};
+use acryl_pdf::{unit::Pt, Document, Vector2};
 
 const FILE_PATH: &'static str = "out/test.pdf";
 
 fn main() {
-
     let mut doc = Document::new();
 
-    doc.add_page(Vector2 { x: 100.into(), y: 100.into() });
+    doc.add_page(Vector2 {
+        x: Pt(100.0),
+        y: Pt(100.0),
+    });
+
 
     let result = doc.render().unwrap();
 
