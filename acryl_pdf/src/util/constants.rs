f75875359577b5807af2451d488cbd2ecd4b149f
brndel <ar.braendle@gmail.com>
2023-07-01 22:24:11 +0200

    Add constants mod Add A4 Page size as a constant

---
+++ acryl_pdf/src/util/constants.rs
@@ -0,0 +1,3 @@
+use crate::{Vector2, unit::Pt};
+
+pub const PAGE_SIZE_A4: Vector2<Pt> = Vector2 { x: Pt(595.0), y: Pt(842.0) };
--- acryl_pdf/src/util/mod.rs
+++ acryl_pdf/src/util/mod.rs
@@ -3,7 +3,10 @@ pub mod unit;
 mod vec;
 mod matrix;
 pub mod color;
+pub mod constants;
 
 pub use area::Area;
 pub use vec::Vector2;
+pub use vec::Vector;
+pub use vec::VectorComponent;
 pub use matrix::Matrix;
