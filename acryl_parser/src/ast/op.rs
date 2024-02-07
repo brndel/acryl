636881f370e9e09538c9afd5e114693ac665738a
brndel <ar.braendle@gmail.com>
2024-02-07 18:56:34 +0100

    Fix missing 'static lifetimes in acryl_parser op.rs

--- acryl_parser/src/ast/op.rs
+++ acryl_parser/src/ast/op.rs
@@ -1,34 +1,34 @@
 pub struct Op;
 
 impl Op {
-    pub const ALLOWED: &str = "+-*/!=~^_<>?#";
+    pub const ALLOWED: &'static str = "+-*/!=~^_<>?#";
 
     // Unary
-    pub const NOT: &str = "!";
-    pub const NEGATIVE: &str = "-";
+    pub const NOT: &'static str = "!";
+    pub const NEGATIVE: &'static str = "-";
 
     // Binary
-    pub const MULTIPLY: &str = "*";
-    pub const DIVIDE: &str = "/";
+    pub const MULTIPLY: &'static str = "*";
+    pub const DIVIDE: &'static str = "/";
 
-    pub const ADD: &str = "+";
-    pub const SUBTRACT: &str = "-";
+    pub const ADD: &'static str = "+";
+    pub const SUBTRACT: &'static str = "-";
 
-    pub const EQUALS: &str = "==";
-    pub const NOT_EQUALS: &str = "!=";
-    pub const GREATER: &str = ">";
-    pub const GREATER_EQUALS: &str = ">=";
-    pub const LESS: &str = "<";
-    pub const LESS_EQUALS: &str = "<=";
+    pub const EQUALS: &'static str = "==";
+    pub const NOT_EQUALS: &'static str = "!=";
+    pub const GREATER: &'static str = ">";
+    pub const GREATER_EQUALS: &'static str = ">=";
+    pub const LESS: &'static str = "<";
+    pub const LESS_EQUALS: &'static str = "<=";
 
     // Other
-    pub const SET: &str = "=";
-    pub const ARROW: &str = "->";
+    pub const SET: &'static str = "=";
+    pub const ARROW: &'static str = "->";
 
     // Binary priority list
-    pub const PRIORITY_HIGH: &[&'static str; 2] = &[Self::MULTIPLY, Self::DIVIDE];
-    pub const PRIORITY_MID: &[&'static str; 2] = &[Self::ADD, Self::SUBTRACT];
-    pub const PRIORITY_LOW: &[&'static str; 6] = &[
+    pub const PRIORITY_HIGH: &'static [&'static str; 2] = &[Self::MULTIPLY, Self::DIVIDE];
+    pub const PRIORITY_MID: &'static [&'static str; 2] = &[Self::ADD, Self::SUBTRACT];
+    pub const PRIORITY_LOW: &'static [&'static str; 6] = &[
         Self::EQUALS,
         Self::NOT_EQUALS,
         Self::GREATER,
