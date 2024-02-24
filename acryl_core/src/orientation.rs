8d3cc78b0aed8a8a99ce725acf8932e157a07f4b
brndel <ar.braendle@gmail.com>
2024-02-24 01:31:30 +0100

    Cleanup Orientation

--- acryl_core/src/orientation.rs
+++ acryl_core/src/orientation.rs
@@ -1,32 +1,28 @@
 use crate::math::{VectorComponent, Vector2};
 
+#[derive(Debug, Default, Clone)]
 pub enum Orientation {
+    #[default]
     Vertical,
     Horizontal,
 }
 
-impl Default for Orientation {
-    fn default() -> Self {
-        Self::Vertical
-    }
-}
-
 impl Orientation {
-    pub fn create_vector<T: VectorComponent>(&self, main: T, cross: T) -> Vector2<T> {
+    pub const fn create_vector<T: VectorComponent>(&self, main: T, cross: T) -> Vector2<T> {
         match self {
             Orientation::Vertical => Vector2::new(cross, main),
             Orientation::Horizontal => Vector2::new(main, cross),
         }
     }
 
-    pub fn get_main<T: VectorComponent>(&self, v: &Vector2<T>) -> T {
+    pub const fn get_main<T: VectorComponent>(&self, v: &Vector2<T>) -> T {
         match self {
             Orientation::Vertical => v.y,
             Orientation::Horizontal => v.x,
         }
     }
 
-    pub fn get_cross<T: VectorComponent>(&self, v: &Vector2<T>) -> T {
+    pub const fn get_cross<T: VectorComponent>(&self, v: &Vector2<T>) -> T {
         match self {
             Orientation::Vertical => v.x,
             Orientation::Horizontal => v.y,
